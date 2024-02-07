b89d8e8eb4b153fa687cb9170da60a04964a2424
aldi <aldiirsanmajid@gmail.com>
2024-02-07 10:43:15 +0700

    first commit

---
+++ number_2.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+main() {
+  print("Enter input data:");
+  String? input = stdin.readLineSync();
+  List<String> clearedInput =
+      input?.replaceAll(" ", "").split("").toList() ?? [];
+
+  var currentLetter = "";
+  var currentLetterCount = 0;
+  List<(String, int)> letterAndCountPair = [];
+
+  // fixme huruf terakhir ga keitung.
+  for (var i = 0; i < clearedInput.length; i++) {
+    if (currentLetter.isNotEmpty && currentLetter != clearedInput[i]) {
+      letterAndCountPair.add((currentLetter, currentLetterCount));
+      currentLetterCount = 0;
+    }
+    currentLetter = clearedInput[i];
+    currentLetterCount = currentLetterCount + 1;
+  }
+
+  // tambahkan huruf terakhir dan total nya ke dalam output
+  if (currentLetter.isNotEmpty) {
+    letterAndCountPair.add((currentLetter, currentLetterCount));
+  }
+
+  print("Output List:");
+  for (var pair in letterAndCountPair) {
+    print("${pair.$1} = ${pair.$2}");
+  }
+}
